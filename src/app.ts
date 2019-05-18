871b7985b2f3863c5ff83210a356b1aeb66d1d2d
Michael Ma <temeraire02@gmail.com>
2019-05-18 12:30:12 -0400

    react app hello world

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { SESSION_SECRET, MONGODB_URI, STEAM_API_KEY } from "./utils/secrets";
 logger.log("debug", SESSION_SECRET + " " + MONGODB_URI + " " + STEAM_API_KEY);
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 
 app.get("/", (request, response) => {
     response.send("warcp");
